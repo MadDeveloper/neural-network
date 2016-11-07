b2544ae2aae8b9a06a5c0445e1fec53d5d2fe7d4
MadDeveloper <sergent.julien@icloud.com>
2016-11-07 14:23:13 +0100

    Edit index.js

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const { Network, Trainer } = require( './brain' )
 
-const Darse     = new Network()
-const trainer   = new Trainer( Darse )
-
-Darse.forge( 2, 4, 1 )
+const net = new Network()
+net.forge( 2, 4, 1 )
 
 console.log(`
-    predictions after training:
-    \t[0,0]: ${Darse.run([ 0, 0 ])[ 0 ]}
-    \t[1,0]: ${Darse.run([ 1, 0 ])[ 0 ]}
-    \t[0,1]: ${Darse.run([ 0, 1 ])[ 0 ]}
-    \t[1,1]: ${Darse.run([ 1, 1 ])[ 0 ]}
+    predictions before training:
+    \t[0,0]: ${net.run([ 0, 0 ])[ 0 ]}
+    \t[1,0]: ${net.run([ 1, 0 ])[ 0 ]}
+    \t[0,1]: ${net.run([ 0, 1 ])[ 0 ]}
+    \t[1,1]: ${net.run([ 1, 1 ])[ 0 ]}
 `)
 
+const trainer = new Trainer( Darse )
 trainer.train([
     { input: [ 0, 0 ], output: [ 0 ], label: '0,0' },
     { input: [ 1, 0 ], output: [ 1 ], label: '1,0' },
@@ -20,15 +19,10 @@ trainer.train([
     { input: [ 1, 1 ], output: [ 0 ], label: '1,1' }
 ])
 
-const predictedXOR00 = Darse.run([ 0, 0 ])
-const predictedXOR10 = Darse.run([ 1, 0 ])
-const predictedXOR01 = Darse.run([ 0, 1 ])
-const predictedXOR11 = Darse.run([ 1, 1 ])
-
 console.log(`
     predictions after training:
-    \t[0,0]: ${Darse.run([ 0, 0 ])[ 0 ]}
-    \t[1,0]: ${Darse.run([ 1, 0 ])[ 0 ]}
-    \t[0,1]: ${Darse.run([ 0, 1 ])[ 0 ]}
-    \t[1,1]: ${Darse.run([ 1, 1 ])[ 0 ]}
+    \t[0,0]: ${net.run([ 0, 0 ])[ 0 ]}
+    \t[1,0]: ${net.run([ 1, 0 ])[ 0 ]}
+    \t[0,1]: ${net.run([ 0, 1 ])[ 0 ]}
+    \t[1,1]: ${net.run([ 1, 1 ])[ 0 ]}
 `)
